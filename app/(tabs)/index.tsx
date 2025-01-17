892cd0f0322354c63d0e238a7343ec9aa7acfe68
Varinder Sharma <92428994+abhaygoswami302@users.noreply.github.com>
2025-01-17 12:47:53 +0530

    add homebanner

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,39 @@
-import { View, Text,StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import React from "react";
+import HomeBanner from "@/assets/images/home-banner.jpg";
 
 const index = () => {
   return (
     <View style={styles.Container}>
-      <Text style={styles.text}>Food Agency</Text>
+      <ImageBackground style={styles.image} source={HomeBanner}>
+        <Text style={styles.text}>Food Agency</Text>
+      </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
-export default index
-const styles=StyleSheet.create({
-Container:{
-  flex:1,
-  flexDirection:'column'
-},
-text:{
-  color:"red",
-  fontSize:42,
-  fontWeight:400,
-}
-})
+export default index;
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  text: {
+    color: "red",
+    fontSize: 42,
+    fontWeight: 400,
+    padding: 5,
+    backgroundColor: "rgba(0,0,0,0.56)",
+    width: "100%",
+    textAlign: "center",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
